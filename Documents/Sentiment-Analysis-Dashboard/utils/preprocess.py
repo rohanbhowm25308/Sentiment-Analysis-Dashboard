ea6109fae01155082bd438b328e12f393ee7dd7a
rohanbhowm25308 <bhowmikrohan83@gmail.com>
2026-07-06 17:41:11 +0530

    Updated project for Render deployment

---
+++ Documents/Sentiment-Analysis-Dashboard/utils/preprocess.py
@@ -0,0 +1,8 @@
+import re
+import string
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(f"[{string.punctuation}]", "", text)
+    text = re.sub(r"\d+", "", text)
+    return text
